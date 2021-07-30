dda02a98501ed31f51905fd4ea1cfd2f9102d6b4
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-07-30 19:41:16 -0300

    main save functions

--- electron/main.js
+++ electron/main.js
@@ -1,23 +1,79 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron')
 const path = require('path')
+const { writeFile } = require('fs')
 
-//require('./header/header-actions-main')
+require('./header/header-actions-main')
+
+const args = process.argv
+
+const argsLenght = process.env.NODE_ENV === 'production' ? 2 : 3
+
+let filePath = args.length == argsLenght ? args[argsLenght - 1] : null
 
 function mainWindow() {
     const win = new BrowserWindow({
-        // icon: path.join(__dirname,"../assets/icon.png"),
+        icon: path.join(__dirname, "../assets/icon.png"),
         frame: false,
         transparent: true,
-        webPreferences:{
+        webPreferences: {
             nodeIntegration: true,
-            //preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js')
         },
     })
-    
+
     win.loadFile('index.html')
 }
 
 app.whenReady().then(mainWindow)
+
+ipcMain.on('save', async (event, arg) => {
+    const { content } = arg
+    let canceled = false
+
+    if (!filePath) {
+        const window = BrowserWindow.getFocusedWindow()
+        const options = {
+            title: "Salvar Como: ",
+            filters: [{ name: 'Arquivo de texto', extensions: ['txt'] }],
+            defaultPath: "document.txt"
+        }
+            ; ({ canceled, filePath } = await dialog.showSaveDialog(window, options))
+    }
+
+    if (canceled) {
+        return
+    }
+
+    console.table({ filePath, canceled })
+
+
+    writeFile(filePath, content, (err, result) => {
+        return 
+    })
+})
+
+ipcMain.on('save-as', async (event, arg) => {
+    const { content } = arg
+
+    const window = BrowserWindow.getFocusedWindow()
+    const options = {
+        title: "Salvar Como: ",
+        filters: [{ name: 'Arquivo de texto', extensions: ['txt'] }],
+        defaultPath: "document.txt"
+    }
+    const { canceled, filePath: newFilePath } = await dialog.showSaveDialog(window, options)
+
+    if (canceled) {
+        return
+    }
+
+    console.table({ newFilePath, canceled })
+
+
+    writeFile(newFilePath, content, (err, result) => {
+        console.log(`Erro: ${err}\nResultado: ${result}`)
+    })
+})
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
